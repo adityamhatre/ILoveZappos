22e0d249cea067941266432f98b826b43eb93909
Aditya <aditya.r.mhatre@gmail.com>
2017-02-09 21:40:11 -0600

    3rd Last Commit

--- app/src/main/java/com/thelegacycoder/ILoveZappos/Activities/MainActivity.java
+++ app/src/main/java/com/thelegacycoder/ILoveZappos/Activities/MainActivity.java
@@ -2,17 +2,19 @@ package com.thelegacycoder.ILoveZappos.Activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.thelegacycoder.ILoveZappos.Adapters.ListViewAdapter;
@@ -33,11 +35,14 @@ public class MainActivity extends AppCompatActivity implements Callback<SearchAP
     private ZapposAPI zapposAPI;
     private ListViewAdapter listViewAdapter;
     private Call<SearchAPIResponse> call;
-    private EditText searchBox;
     private View searchView;
     private Boolean firstTime = true;
     private List<ProductItem> productItemList;
     private View listContainer;
+    private SearchView searchProductView;
+    private String searchQuery;
+    private boolean sharing;
+    private int index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,11 +55,23 @@ public class MainActivity extends AppCompatActivity implements Callback<SearchAP
 
         Uri data = getIntent().getData();
         if (data != null) {
+            sharing = false;
             String url = data.toString();
-            url = url.replace("http://www.zappos.com/zappos/", "");
-            System.out.println(url);
-            searchProduct(url);
-        }
+            if (url.contains("share.zappos.com")) sharing = true;
+            url = url.replace("http://share.zappos.com/zappos/", "");
+            String searchQuery = url.substring(0, url.indexOf("&"));
+            searchQuery = searchQuery.replace("search=", "");
+            String index = url.substring(url.indexOf("&") + 1, url.length());
+            index = index.replace("index=", "");
+            try {
+                if (sharing)
+                    searchProduct(searchQuery);
+                this.index = Integer.parseInt(index);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else sharing = false;
 
 
         if (savedInstanceState != null && savedInstanceState.containsKey("search")) {
@@ -62,55 +79,90 @@ public class MainActivity extends AppCompatActivity implements Callback<SearchAP
         }
     }
 
-    private void initActionListeners() {
-        searchBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int actionID, KeyEvent keyEvent) {
-                if (actionID == EditorInfo.IME_ACTION_SEARCH) {
-                    searchProduct(searchBox.getText().toString());
-                    ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(searchBox.getWindowToken(), 0);
-                }
+    private void initViews() {
+        gridView = (GridView) findViewById(R.id.grid_view);
+        searchView = findViewById(R.id.search_view);
+        listContainer = findViewById(R.id.list_container);
 
+        searchProductView = new SearchView(this);
+        searchProductView.setSubmitButtonEnabled(true);
+        searchProductView.setQueryHint("Search");
 
-                return false;
-            }
-        });
+        ((EditText) searchProductView.findViewById(android.support.v7.appcompat.R.id.search_src_text)).setTextColor(Color.WHITE);
+        ((EditText) searchProductView.findViewById(android.support.v7.appcompat.R.id.search_src_text)).setHintTextColor(Color.GRAY);
+        ImageView imageView = ((ImageView) searchProductView.findViewById(android.support.v7.appcompat.R.id.search_button));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        imageView.setImageResource(R.drawable.ic_search);
 
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                startActivity(new Intent(MainActivity.this, ProductViewActivity.class).putExtra("productIndex", i));
-            }
-        });
-
-        findViewById(R.id.search_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                searchProduct(searchBox.getText().toString());
-                ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(view.getWindowToken(), 0);
-            }
-        });
+        hideContainer();
     }
 
     private void initAPI() {
         zapposAPI = AppController.getInstance().getRetrofit().create(ZapposAPI.class);
     }
 
+    private void initActionListeners() {
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (sharing) {
+                    startActivityForResult(new Intent(MainActivity.this, ProductViewActivity.class).putExtra("productIndex", i).putExtra("searchTerm", searchQuery).putExtra("sharing", sharing), 212);
+                } else {
+                    startActivity(new Intent(MainActivity.this, ProductViewActivity.class).putExtra("productIndex", i).putExtra("searchTerm", searchQuery).putExtra("sharing", sharing));
+                }
+                if (sharing) sharing = false;
+            }
+        });
+
+
+        searchProductView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchQuery = query;
+                searchProductView.clearFocus();
+                hideKeyboard(searchProductView);
+                searchProduct(query);
+                searchProductView.onActionViewCollapsed();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
 
     private void searchProduct(String searchTerm) {
-        if (!searchTerm.isEmpty()) {
+        if (searchTerm != null && !searchTerm.isEmpty()) {
             call = zapposAPI.searchQuery(searchTerm, getString(R.string.zappos_key));
             AppController.getInstance().showLoading(this);
             call.enqueue(this);
         }
     }
 
-    private void initViews() {
-        gridView = (GridView) findViewById(R.id.grid_view);
-        searchBox = (EditText) findViewById(R.id.search_box);
-        searchView = findViewById(R.id.search_view);
-        listContainer = findViewById(R.id.list_container);
-        hideContainer();
+    private void hideContainer() {
+        listContainer.setVisibility(View.GONE);
+        searchView.setVisibility(View.VISIBLE);
+    }
+
+    private void showContainer() {
+        listContainer.setVisibility(View.VISIBLE);
+        searchView.setVisibility(View.GONE);
+    }
+
+    private void hideKeyboard(View view) {
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 212 && resultCode == 200) {
+            hideContainer();
+        }
+
     }
 
     @Override
@@ -129,10 +181,13 @@ public class MainActivity extends AppCompatActivity implements Callback<SearchAP
                 productItemList.addAll(response.body().getResults());
                 listViewAdapter.notifyDataSetChanged();
             }
+
             showContainer();
+            if (sharing) {
+                gridView.performItemClick(gridView.getChildAt(index), index, gridView.getAdapter().getItemId(index));
+            }
         } else {
             Toast.makeText(this, "No results", Toast.LENGTH_SHORT).show();
-            searchView.setVisibility(View.VISIBLE);
             if (productItemList != null)
                 productItemList.clear();
             if (listViewAdapter != null) {
@@ -145,24 +200,21 @@ public class MainActivity extends AppCompatActivity implements Callback<SearchAP
         AppController.getInstance().dismissLoading();
     }
 
-    private void hideContainer() {
-        listContainer.setVisibility(View.GONE);
-    }
-
-    private void showContainer() {
-        listContainer.setVisibility(View.VISIBLE);
-    }
-
     @Override
     public void onFailure(Call<SearchAPIResponse> call, Throwable t) {
         AppController.getInstance().dismissLoading();
         Toast.makeText(this, "Cannot connect to server", Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Search").setActionView(searchProductView).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        outState.putString("search", searchBox.getText().toString());
+        outState.putString("search", searchQuery);
         super.onSaveInstanceState(outState);
     }
 }
--- app/src/main/java/com/thelegacycoder/ILoveZappos/Activities/ProductViewActivity.java
+++ app/src/main/java/com/thelegacycoder/ILoveZappos/Activities/ProductViewActivity.java
@@ -1,12 +1,14 @@
 package com.thelegacycoder.ILoveZappos.Activities;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AlphaAnimation;
@@ -31,28 +33,34 @@ import retrofit2.Response;
 
 public class ProductViewActivity extends AppCompatActivity implements Callback<ProductAPIResponse> {
 
-    Toolbar toolbar;
-    Boolean isAnimating = false;
+    private Toolbar toolbar;
+    private Boolean isAnimating = false;
     private Call<ProductAPIResponse> call;
-    ActivityProductViewBinding binding;
-    ProductItem productItem;
-    DetailedProductItem detailedProductItem;
+    private ActivityProductViewBinding binding;
+    private ProductItem productItem;
+    private DetailedProductItem detailedProductItem;
+
+    private int productIndex;
+    private String searchTerm;
+    private Boolean sharing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_product_view);
 
+        initViews();
+        initListeners();
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        int productIndex = getIntent().getIntExtra("productIndex", 0);
-
-        productItem = AppController.getInstance().getProducts().get(productIndex);
-
-        detailedProductItem = new DetailedProductItem();
-        detailedProductItem.setDefaultImageUrl("http://www.zappos.com/images/z/3/5/2/8/1/9/3528199-p-DETAILED.jpg");
+        try {
+            productIndex = getIntent().getIntExtra("productIndex", 0);
+            searchTerm = getIntent().getStringExtra("searchTerm");
+            sharing = getIntent().getBooleanExtra("sharing", false);
+            productItem = AppController.getInstance().getProducts().get(productIndex);
+            detailedProductItem = new DetailedProductItem();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
         getHDImage(productItem.getProductId());
@@ -66,6 +74,20 @@ public class ProductViewActivity extends AppCompatActivity implements Callback<P
             findViewById(R.id.productSaved).setVisibility(View.VISIBLE);
         }
 
+
+    }
+
+    private void initViews() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    private void initListeners() {
         findViewById(R.id.add_to_cart).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -90,8 +112,8 @@ public class ProductViewActivity extends AppCompatActivity implements Callback<P
     }
 
     private void scaleAndTranslate() {
-        final View view = findViewById(R.id.copy);
-        final View fab = findViewById(R.id.add_to_cart);
+        final View productImageCopy = findViewById(R.id.copy_productImage);
+        final View floatingActionButton = findViewById(R.id.add_to_cart);
 
         AnimationSet animationSet = new AnimationSet(true);
         animationSet.setFillAfter(false);
@@ -101,14 +123,13 @@ public class ProductViewActivity extends AppCompatActivity implements Callback<P
 
         TranslateAnimation translate = new TranslateAnimation(
                 0,
-                fab.getX() - view.getX() + fab.getPaddingLeft(),
+                floatingActionButton.getX() - productImageCopy.getX() + floatingActionButton.getPaddingLeft(),
                 0,
-                fab.getY() - view.getY() + fab.getPaddingBottom());
+                floatingActionButton.getY() - productImageCopy.getY() + floatingActionButton.getPaddingBottom());
         ScaleAnimation scale = new ScaleAnimation(1f, .2f, 1f, .2f);
-        AlphaAnimation alphaAnimation = new AlphaAnimation(1, 0);
+        AlphaAnimation alpha = new AlphaAnimation(1, 0);
 
-
-        animationSet.addAnimation(alphaAnimation);
+        animationSet.addAnimation(alpha);
         animationSet.addAnimation(scale);
         animationSet.addAnimation(translate);
 
@@ -121,7 +142,7 @@ public class ProductViewActivity extends AppCompatActivity implements Callback<P
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                view.setVisibility(View.INVISIBLE);
+                productImageCopy.setVisibility(View.INVISIBLE);
                 isAnimating = false;
                 Toast.makeText(ProductViewActivity.this, "Added to cart", Toast.LENGTH_SHORT).show();
 
@@ -132,14 +153,14 @@ public class ProductViewActivity extends AppCompatActivity implements Callback<P
 
             }
         });
-        view.startAnimation(animationSet);
+        productImageCopy.startAnimation(animationSet);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 RotateAnimation rotateAnimation = new RotateAnimation(0, -360f, Animation.RELATIVE_TO_SELF, .5f, Animation.RELATIVE_TO_SELF, 0.5f);
                 rotateAnimation.setDuration(500);
-                fab.startAnimation(rotateAnimation);
+                floatingActionButton.startAnimation(rotateAnimation);
             }
         }, 300);
 
@@ -147,10 +168,21 @@ public class ProductViewActivity extends AppCompatActivity implements Callback<P
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //menu.add("Cart").setIcon(R.drawable.ic_cart).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-        return true;
+        menu.add("Share").setIcon(android.R.drawable.ic_menu_share).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                String share = "Check out this product on Zappos: " + productItem.getProductName();
+                share += ("\n\nhttp://share.zappos.com/zappos/search=" + searchTerm + "&index=" + productIndex + "");
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, share);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+                return false;
+            }
+        }).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        return super.onCreateOptionsMenu(menu);
     }
-
 
     @Override
     public void onResponse(Call<ProductAPIResponse> call, Response<ProductAPIResponse> response) {
@@ -158,15 +190,30 @@ public class ProductViewActivity extends AppCompatActivity implements Callback<P
         AppController.getInstance().dismissLoading();
         try {
             detailedProductItem.setDefaultImageUrl(response.body().getProduct().get(0).getDefaultImageUrl());
-
             binding.setProduct(productItem);
             binding.setDetailedProduct(detailedProductItem);
 
         } catch (Exception e) {
+            Toast.makeText(this, "Some error occurred", Toast.LENGTH_SHORT).show();
             onBackPressed();
             e.printStackTrace();
         }
 
+
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (sharing)
+            setResult(200);
+        super.onBackPressed();
 
     }
 
